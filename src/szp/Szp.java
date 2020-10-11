fb7a9dd3aa898cfa98a3044ed194a7f8ec8ea03a
29778 <2977898490@qq.com>
2020-10-11 15:57:09 +0800

    110

---
+++ src/szp/Szp.java
@@ -0,0 +1,17 @@
+package szp;
+
+import java.util.*;
+
+public class Szp
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("请输入数字a");
+		int a=scanner.nextInt();
+		System.out.println("请输入数字b");
+		int b=scanner.nextInt();
+		int sum=a+b;
+		System.out.println("两个数之和为"+sum);
+	}
+}
